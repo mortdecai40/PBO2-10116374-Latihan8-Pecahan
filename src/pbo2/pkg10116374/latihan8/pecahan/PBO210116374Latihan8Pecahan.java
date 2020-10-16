7a6fb85eb3535001436d2120eca57e0d42973bd3
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 14:07:51 +0700

    Program bilangan Pecahan

---
+++ src/pbo2/pkg10116374/latihan8/pecahan/PBO210116374Latihan8Pecahan.java
@@ -0,0 +1,25 @@
+package pbo2.pkg10116374.latihan8.pecahan;
+
+/**
+ * @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk memasukan bilangan Pecahan
+ */
+public class PBO210116374Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float \t: "+ f);
+        System.out.println("Double \t: "+ d);
+                
+    }
+    
+}
